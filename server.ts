d16697764fd15815acdba7ee545302075e68df82
BigFudge420 <87513995+BigFudge420@users.noreply.github.com>
2025-12-26 09:36:53 +0530

    feat : graceful shutdown on terminate or interrupt

--- server.ts
+++ server.ts
@@ -1,6 +1,18 @@
 import app from "./src/app";
 import config from './src/config'
+import prisma from "./src/prisma";
 
-app.listen(config.port, () => {
+const server = app.listen(config.port, () => {
     console.log(`Server listening on port ${config.port}`)
 })
+
+const shutdown = async () => {
+  console.log("Shutting down server...")
+  await prisma.$disconnect()
+  server.close(() => {
+    process.exit(0)
+  })
+}
+
+process.on("SIGINT", shutdown)
+process.on("SIGTERM", shutdown)
